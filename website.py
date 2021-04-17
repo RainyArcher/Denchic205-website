538ac65c2da79992785abf214db3c81ac77672a2
Denchic205 <denchiczerofive20@gmail.com>
2021-04-18 00:00:30 +0300

    Now you can delete comments, but you can also encounter an error 505 when uploading it. It'll be fixed soon, I promise :)

--- website.py
+++ website.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request, abort
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 import datetime
-import filecmp
 import random
 import os
 from data import db_session
@@ -41,9 +40,59 @@ def login():
     return render_template('login.html', title='Авторизация', deletion=False, form=form)
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_session.global_init("db/users.db")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        print('Adding user...')
+        if not form.avatar.data:
+            avatar_name = avatar_function('')
+        else:
+            avatar = form.avatar.data
+            if avatar.filename.split('.')[-1] not in ['png', 'jpeg', 'jpg', 'ico', 'gif', 'bmp']:
+                return render_template('register.html', title='Регистрация',
+                                       form=form,
+                                       message="Файл не является изображением")
+            avatar_name = 'Avatar_' + form.email.data + '_' + str(datetime.datetime.now()).replace(":", "-") + \
+                          '.' + avatar.filename.split('.')[-1]
+            avatar.save("static/img/Avatars/" + avatar_name)
+        if not form.age.data.isnumeric() or int(form.age.data) > 200:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пожалуйста, введите корректный возраст")
+        if int(form.age.data) < 12:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Простите, на сайте действует возрастное ограничение 12+")
+        user = User(
+            surname=form.surname.data,
+            name=form.name.data,
+            age=form.age.data,
+            address=form.address.data,
+            email=form.email.data,
+            avatar=avatar_name,
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        print('User added')
+        logout_user()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @app.route("/")
 def index():
-    filecmp.clear_cache()
     db_sess = db_session.create_session()
     posts = db_sess.query(Posts).all()
     users = db_sess.query(User).all()
@@ -51,7 +100,7 @@ def index():
     count = 0
     all_private = False
     for post in posts:
-        if post.is_private:
+        if post.is_private and post.user != current_user:
             count += 1
     if len(posts) == count:
         all_private = True
@@ -112,6 +161,14 @@ def settings(id):
                                        message="Введите предыдущий пароль, чтобы продолжить")
             user.surname = form.surname.data
             user.name = form.name.data
+            if not form.age.data.isnumeric() or int(form.age.data) > 200:
+                return render_template('settings.html', title='Настройки',
+                                       form=form,
+                                       message="Пожалуйста, введите корректный возраст")
+            if int(form.age.data) < 12:
+                return render_template('settings.html', title='Настройки',
+                                       form=form,
+                                       message="Простите, на сайте действует возрастное ограничение 12+")
             user.age = form.age.data
             user.address = form.address.data
             user.avatar = avatar_name
@@ -121,49 +178,6 @@ def settings(id):
         else:
             abort(404)
     return render_template('settings.html', title='Настройки', form=form)
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def reqister():
-    form = RegisterForm()
-    if form.validate_on_submit():
-        if form.password.data != form.password_again.data:
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Пароли не совпадают")
-        db_session.global_init("db/users.db")
-        db_sess = db_session.create_session()
-        if db_sess.query(User).filter(User.email == form.email.data).first():
-            return render_template('register.html', title='Регистрация',
-                                   form=form,
-                                   message="Такой пользователь уже есть")
-        print('Adding user...')
-        if not form.avatar.data:
-            avatar_name = avatar_function('')
-        else:
-            avatar = form.avatar.data
-            if avatar.filename.split('.')[-1] not in ['png', 'jpeg', 'jpg', 'ico', 'gif', 'bmp']:
-                return render_template('register.html', title='Регистрация',
-                                       form=form,
-                                       message="Файл не является изображением")
-            avatar_name = 'Avatar_' + form.email.data + '_' + str(datetime.datetime.now()).replace(":", "-") + \
-                          '.' + avatar.filename.split('.')[-1]
-            avatar.save("static/img/Avatars/" + avatar_name)
-        user = User(
-            surname=form.surname.data,
-            name=form.name.data,
-            age=form.age.data,
-            address=form.address.data,
-            email=form.email.data,
-            avatar=avatar_name,
-        )
-        user.set_password(form.password.data)
-        db_sess.add(user)
-        db_sess.commit()
-        print('User added')
-        logout_user()
-        return redirect('/login')
-    return render_template('register.html', title='Регистрация', form=form)
 
 
 @app.route('/logout')
@@ -181,16 +195,17 @@ def delete_user(id):
     if user and user == current_user:
         form = LoginForm()
         if form.validate_on_submit():
-            db_sess = db_session.create_session()
             if user and user.check_password(form.password.data) and user.email == current_user.email:
                 for post in user.posts:
                     if os.path.isfile('static/img/Posts/' + post.image) and post.image != 'Empty.png':
                         os.remove('static/img/Posts/' + post.image)
-                    # for comment in user.comments:
-                    # db_sess.delete(comment)
+                    for comment in post.comments:
+                        db_sess.delete(comment)
                     db_sess.delete(post)
-                    db_sess.commit()
                 print('All posts deleted')
+                for comment in user.comments:
+                    db_sess.delete(comment)
+                print('All comments deleted')
                 if avatar_function(user.avatar) and os.path.isfile('static/img/Avatars/' + user.avatar):
                     os.remove('static/img/Avatars/' + user.avatar)
                 db_sess.delete(user)
@@ -210,17 +225,20 @@ def add_comment(id):
     form = CommentsForm()
     db_sess = db_session.create_session()
     post = db_sess.query(Posts).filter(Posts.id == id).first()
-    if form.validate_on_submit():
-        comment = Comments()
-        comment.text = form.text.data
-        comment.post_id = id
-        current_user.comments.append(comment)
-        db_sess.merge(current_user)
-        db_sess.merge(db_sess.query(Posts).filter(Posts.id == id).first())
-        db_sess.commit()
-        return redirect(f'/post/{id}')
-    return render_template('Comment.html', title='Добавление комментария',
-                           form=form, post=post)
+    if not post.is_private or (post.is_private and post.user == current_user):
+        if form.validate_on_submit():
+            comment = Comments()
+            comment.text = form.text.data
+            comment.post_id = id
+            current_user.comments.append(comment)
+            db_sess.merge(current_user)
+            db_sess.merge(db_sess.query(Posts).filter(Posts.id == id).first())
+            db_sess.commit()
+            return redirect(f'/post/{id}')
+        return render_template('Comment.html', title='Добавление комментария',
+                               form=form, post=post)
+    else:
+        abort(404)
 
 
 @app.route('/addPost', methods=['GET', 'POST'])
@@ -330,13 +348,27 @@ def post_delete(id):
     return redirect('/')
 
 
+@app.route('/comment_delete/<int:id>', methods=['GET', 'POST'])
+def comment_delete(id):
+    db_sess = db_session.create_session()
+    comment = db_sess.query(Comments).filter(Comments.id == id).first()
+    post_id = comment.post_id
+    if comment and current_user == comment.user:
+        db_sess.delete(comment)
+        db_sess.commit()
+    else:
+        abort(404)
+    if post:
+        return redirect(f'/post/{post_id}')
+
+
 @app.route('/post/<int:id>')
 @login_required
 def post(id):
     db_sess = db_session.create_session()
     post = db_sess.query(Posts).filter(Posts.id == id).first()
     post_comments = db_sess.query(Comments).filter(Comments.post_id == id).all()
-    if post:
+    if post and (not post.is_private or (post.is_private and current_user == post.user)):
         return render_template('Post.html',
                                title=f'Post {post.id}', post=post, post_comments=post_comments)
     else:
